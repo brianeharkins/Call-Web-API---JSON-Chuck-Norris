282da5e4cea5cec02971257156fd1038a9764961
Brian Harkins <brianharkins@outlook.com>
2021-08-22 21:02:24 +0100

    Add project files.

---
+++ WebApiJsonDeserialise.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+
+namespace Call_Web_API_1
+{
+    class WebApiJsonDeserialise
+    {
+        // Add nuget package Newtonsoft.json
+        static void Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            Console.WriteLine("Calling WEBAPI ....");
+                       
+            var responseTask = client.GetAsync("https://api.chucknorris.io/jokes/random");   // Random Chuck Norris joke
+
+            responseTask.Wait();
+
+            if (responseTask.IsCompleted)
+            {
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var jsonData = result.Content.ReadAsStringAsync();
+                    jsonData.Wait();
+                    var strJsonData = jsonData.Result.ToString();
+
+                    // Now we need to deserialise this JSON string
+                    var jsonChuckNorris = JsonConvert.DeserializeObject<jsonJoke>(strJsonData);    // dynamic means it is resolved at runtime. Will use own class soon
+
+                    Console.WriteLine("This is our JSON object : " + jsonChuckNorris.ToString());
+
+                    Console.WriteLine("");
+                    Console.WriteLine("icon_url : " + jsonChuckNorris.icon_url);
+                    Console.WriteLine("url : " + jsonChuckNorris.url);
+                    Console.WriteLine("value : " + jsonChuckNorris.value);
+                    Console.WriteLine("updated at : " + jsonChuckNorris.updated_at);
+                }
+            }            
+            
+            // Thanks to: https://www.youtube.com/watch?v=CjoAYslTKX0&ab_channel=LesJackson
+           
+        }       
+    }
+}
---
+++ jsonJoke.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Call_Web_API_1
+{
+
+//    {
+//  "categories": [],
+//  "created_at": "2020-01-05 13:42:26.447675",
+//  "icon_url": "https://assets.chucknorris.host/img/avatar/chuck-norris.png",
+//  "id": "O9R2JBx9TAC2bbaurxrwzg",
+//  "updated_at": "2020-01-05 13:42:26.447675",
+//  "url": "https://api.chucknorris.io/jokes/O9R2JBx9TAC2bbaurxrwzg",
+//  "value": "Chuck Norris can piss into the wind and not get wet."
+//  }
+
+class jsonJoke
+    {
+        public DateTime created_at { get; set; }
+        public string icon_url { get; set; }
+        public string id { get; set; }
+        public DateTime updated_at { get; set; }
+        public string url { get; set; }
+        public string value { get; set; }
+
+    }
+}
